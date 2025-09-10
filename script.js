a8c4f675f88a66617e6fda89b71c1e924a470a53
Danulo4342 <kindijdanilo@gmail.com>
2025-09-10 09:45:06 +0300

    js

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+let celsius = "20";
+let fahrenheit = celsius * 1.8;
+console.log(fahrenheit);
+
+let daysInMonth = 31;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = daysInMonth * 24 * 60;
+console.log(daysInMonth, hoursInMonth, minutesInMonth);
+
+let health = 10;
+let energy = 10;
+let healthAfter = health - 3;
+let energyAfter = energy - 2;
+console.log("health", healthAfter, "energy", energyAfter);
+
+let totalPrice = 500;
+let discount = 0.1;
+let discountedPrice = totalPrice * discount;
+console.log(discountedPrice);
+
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+const floatString = "45.67";
+const parsedFloat = (Number.parseFloat(floatString));
+console.log(parsedFloat);
+
+const intString = "123";
+const parsedInt = (Number.parseInt(intString));
+console.log(parsedInt);
+
+const number = 2;
+const sqrtNumber = Math.sqrt(Math.pow(number, 4));
+console.log(sqrtNumber);
